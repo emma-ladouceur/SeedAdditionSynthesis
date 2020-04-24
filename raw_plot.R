66709256ffa1472d9e3b9b941f9d3da9c5923ff1
Emma Ladouceur <emma.ladouceur@idiv.de>
2020-04-24 10:13:41 +0200

    raw plot

--- raw_plot.R
+++ raw_plot.R
@@ -24,6 +24,8 @@ colnames(plot)
 
 plot$Treatment<-plot$trt
 
+View(plot)
+
 # use grid_arrange_shared_legend function at the beginning of Main_Analysis.R
 # to create figures
 
@@ -32,7 +34,7 @@ plot$Study<-revalue(plot$Experiment_, c("ASGA_Michigan"="Michigan.us", "Californ
 plot$Study<-factor(as.character(plot$Study))
 
 
-rp<-ggplot(plot,aes(x=Study, y=rich.plot,color=Treatment)) +
+rp<-ggplot(plot,aes(x=Study, y=rich.plot)) +
 geom_jitter( aes(shape = Treatment,color = Treatment),
              position = position_jitterdodge(jitter.width = 0.4,jitter.height = 0.4, dodge.width = 0.8), alpha=0.4,size=1.2) +
   stat_summary(aes(shape=Treatment),
@@ -42,6 +44,14 @@ geom_jitter( aes(shape = Treatment,color = Treatment),
   scale_color_manual(values =  c("#A1C720FF","#15983DFF"))  + 
   theme_classic()+theme(axis.text.x = element_text(size=9, angle=7), plot.margin=margin(t=4,1,1,1, "lines"),
                         legend.direction = "horizontal", legend.position = c(0.5,1.2) )+
+  # geom_text(data = plot %>%
+  #             group_by(Study) %>%
+  #             dplyr::mutate(n_plots = n_distinct(unique.id_)) %>%
+  #             ungroup() %>%
+  #             distinct(Study, n_plots, .keep_all = T),
+  #           aes(x=Study, y=50,
+  #               label=paste('n[plots] == ', n_plots)),
+  #           size=3.5,nudge_y = 0.1, parse = T) +
   labs(title = "a) Plot Species Richness") + ylab("Species Richness") 
 
 bp<-ggplot(plot,aes(x= Study, y= biomass.plot)) +
@@ -53,10 +63,10 @@ bp<-ggplot(plot,aes(x= Study, y= biomass.plot)) +
                position = position_dodge(0.8)) + ylim(0,1100) +
   scale_color_manual(values =  c("#A1C720FF","#15983DFF")) + 
   theme_classic()+theme(axis.text.x = element_text(size=9, angle=7),legend.position="none") +
+
   labs(title = 'b) Plot Biomass')  + ylab(expression(paste('Biomass (g/',m^2, ')'))) 
 
 
+
 (rp)/(bp)
-
-
 
